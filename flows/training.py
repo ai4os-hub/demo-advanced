241bc413aaff2e44b5fce8eb36b59adffe31da8e
Borja Esteban <boressan@outlook.com>
2023-06-22 12:55:30 +0200

    add train_request flow task

--- flows/training.py
+++ flows/training.py
@@ -1,3 +1,5 @@
+import uuid
+
 import httpx
 import prefect
 
@@ -7,6 +9,11 @@ def train_model(
     model_uri: str = "models:/borja-MNIST/1",
     dataset: str = "t10k-dataset.npz",
 ):
+    train_request(model_uri, dataset)
+
+
+@prefect.task
+def train_request(model_uri: str, dataset: str) -> uuid.UUID:
     logger = prefect.get_run_logger()
     response = httpx.post(
         "http://localhost:5000/v2/models/deepaas_full/train/",
@@ -14,6 +21,7 @@ def train_model(
     )
     response.raise_for_status()
     logger.info("Response: %s", response.json())
+    return response.json()["uuid"]
 
 
 if __name__ == "__main__":
