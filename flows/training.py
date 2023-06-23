5bd959453499f53d9dec7b79325b9ce83a234ee9
Borja Esteban <boressan@outlook.com>
2023-06-23 08:49:06 +0200

    add get_training task

--- flows/training.py
+++ flows/training.py
@@ -1,4 +1,5 @@
-import uuid
+import time
+from uuid import UUID
 
 import httpx
 import prefect
@@ -8,12 +9,22 @@ import prefect
 def train_model(
     model_uri: str = "models:/borja-MNIST/1",
     dataset: str = "t10k-dataset.npz",
+    max_retries: int = 10,
 ):
-    train_request(model_uri, dataset)
+    uuid = post_training(model_uri, dataset)
+    for retry in range(max_retries):
+        training = get_training(uuid)
+        if training["status"] == "running":
+            time.sleep(10)
+        else:
+            break
+
+    if retry >= 10:
+        raise TimeoutError("Training timeout")
 
 
 @prefect.task
-def train_request(model_uri: str, dataset: str) -> uuid.UUID:
+def post_training(model_uri: str, dataset: str) -> UUID:
     logger = prefect.get_run_logger()
     response = httpx.post(
         "http://localhost:5000/v2/models/deepaas_full/train/",
@@ -22,6 +33,17 @@ def train_request(model_uri: str, dataset: str) -> uuid.UUID:
     response.raise_for_status()
     logger.info("Response: %s", response.json())
     return response.json()["uuid"]
+
+
+@prefect.task
+def get_training(uuid: UUID):
+    logger = prefect.get_run_logger()
+    response = httpx.get(
+        f"http://localhost:5000/v2/models/deepaas_full/train/{uuid}",
+    )
+    response.raise_for_status()
+    logger.info("Response: %s", response.json())
+    return response.json()
 
 
 if __name__ == "__main__":
