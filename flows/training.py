329e258766c20464599d8b4f7f9c5bc77a706e2b
Borja Esteban <boressan@outlook.com>
2023-06-27 09:46:40 +0200

    add prefect deployment

--- flows/training.py
+++ flows/training.py
@@ -1,31 +1,9 @@
 """Training workflow for MNIST model.
 """
-import argparse
-import sys
-
 import prefect
 from prefect.task_runners import SequentialTaskRunner
 
 import api
-
-# Script arguments definition ---------------------------------------
-parser = argparse.ArgumentParser(
-    prog="PROG",
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    epilog="See '<command> --help' to read about a specific sub-command.",
-)
-parser.add_argument(
-    "model_uri",
-    help="Model URI from MLFlow to use for training.",
-    type=str,
-)
-parser.add_argument(
-    "dataset",
-    help="Dataset to use for training.",
-    type=str,
-)
-# -------------------------------------------------------------------
 
 
 @prefect.flow(
@@ -33,7 +11,7 @@ parser.add_argument(
     description="Flow to train a MNIST model.",
     task_runner=SequentialTaskRunner(),
 )
-def main(model_uri, dataset, **options):
+def main(model_uri, dataset):
     train(model_uri, dataset)
 
 
@@ -43,9 +21,3 @@ def train(model_uri, dataset, **options):
     result = api.train(model_uri, dataset, **options)
     logger.debug("Response: %s", result)
     return result
-
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-    main(**vars(args))
-    sys.exit(0)  # Shell return 0 == success
