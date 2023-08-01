41fbb96a7898a0e4d22e9ef64a606f03794ecef0
Borja Esteban <boressan@outlook.com>
2023-08-01 16:30:10 +0200

    merge config into one config

--- deepaas_full/config.py
+++ deepaas_full/config.py
@@ -1,9 +1,18 @@
 """Configuration loader for model source."""
 import os
+from importlib.metadata import metadata
+from pathlib import Path
+
+# Configuration of paths and model metadata
+MODELS_PATH = Path(os.getenv("MODELS_PATH", default="models"))
+DATA_PATH = Path(os.getenv("DATA_PATH", default="data"))
+MODEL_NAME = os.getenv("MODEL_NAME", default="deepaas_full")
+MODEL_METADATA = metadata(MODEL_NAME).json
 
 # Configuration of `models` path as default store for models
-MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", default=None)
-os.environ["MLFLOW_TRACKING_URI"] = MLFLOW_TRACKING_URI or "models"
+_LOCAL_URI = str(Path.cwd() / MODELS_PATH)
+_MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", default=_LOCAL_URI)
+os.environ["MLFLOW_TRACKING_URI"] = _MLFLOW_TRACKING_URI
 
 # Configuration of model framework features
 LABEL_DIMENSIONS = int(os.getenv("LABEL_DIMENSIONS", default="10"))
