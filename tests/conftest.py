65daa96a08ca3b9e4091eeb018d954c8f0af4513
Borja Esteban <boressan@outlook.com>
2023-08-02 15:04:38 +0200

    simplification of test_prediction configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,26 +2,26 @@
 fixtures to simplify model and api specific testing. 
 """
 # pylint: disable=redefined-outer-name
+import inspect
 import os
 import shutil
 import tempfile
 
 import pytest
 
-# Project model configuration was imported inside the api as config
-from api import config
+import api
 
 
 @pytest.fixture(scope="session", autouse=True)
 def original_datapath():
     """Fixture to generate a original directory path for datasets."""
-    return config.DATA_PATH.absolute()
+    return api.config.DATA_PATH.absolute()
 
 
 @pytest.fixture(scope="session", autouse=True)
 def original_modelspath():
     """Fixture to generate a original directory path for datasets."""
-    return config.MODELS_PATH.absolute()
+    return api.config.MODELS_PATH.absolute()
 
 
 @pytest.fixture(scope="module", name="testdir")
@@ -35,10 +35,42 @@ def create_testdir():
 @pytest.fixture(scope="module", autouse=True)
 def copytree_data(testdir, original_datapath):
     """Fixture to copy the original data directory to the test directory."""
-    shutil.copytree(original_datapath, f"{testdir}/{config.DATA_PATH}")
+    shutil.copytree(original_datapath, f"{testdir}/{api.config.DATA_PATH}")
 
 
 @pytest.fixture(scope="module", autouse=True)
 def copytree_models(testdir, original_modelspath):
     """Fixture to copy the original models directory to the test directory."""
-    shutil.copytree(original_modelspath, f"{testdir}/{config.MODELS_PATH}")
+    shutil.copytree(original_modelspath, f"{testdir}/{api.config.MODELS_PATH}")
+
+
+def generate_signature(names, kind=inspect.Parameter.POSITIONAL_OR_KEYWORD):
+    """Function to generate dynamically signatures."""
+    parameters = [inspect.Parameter(name, kind) for name in names]
+    return inspect.Signature(parameters=parameters)
+
+
+def generate_fields_fixture(signature):
+    """Function to generate dynamically fixtures."""
+    def fixture_function(**options):  # fmt: skip
+        return {k: v for k, v in options.items() if v is not None}
+    fixture_function.__signature__ = signature
+    return pytest.fixture(scope="module")(fixture_function)
+
+
+# Generate and inject fixtures for predict arguments
+fields_predict = api.schemas.PredArgsSchema().fields
+signature = generate_signature(fields_predict.keys())
+globals()["predict_kwds"] = generate_fields_fixture(signature)
+
+
+@pytest.fixture(scope="module")
+def metadata():
+    """Fixture to return defined api metadata."""
+    return api.get_metadata()
+
+
+@pytest.fixture(scope="module")
+def predictions(predict_kwds):
+    """Fixture to return predictions to assert properties."""
+    return api.predict(**predict_kwds)
--- tests/test_predictions/conftest.py
+++ tests/test_predictions/conftest.py
@@ -6,21 +6,22 @@ from deepaas.model.v2.wrapper import UploadedFile
 import api
 
 
-@pytest.fixture(scope="module", params=["models:/deepaas_full-testing/1"])
-def model_uri(request):
-    """Fixture to provide the model_uri argument to api.predict."""
-    return request.param
-
-
 @pytest.fixture(scope="module", params=["t100-images.npy"])
 def input_file(request):
     """Fixture to provide the input_file argument to api.predict."""
-    return UploadedFile("", filename=api.config.DATA_PATH / request.param)
+    filepath = api.config.DATA_PATH / "interim"
+    return UploadedFile("", filename=filepath / request.param)
 
 
-@pytest.fixture(scope="module", params=["application/json"])
-def accept(request):
-    """Fixture to provide the accept argument to api.predict."""
+@pytest.fixture(scope="module", params=["test_simplemodel"])
+def model_name(request):
+    """Fixture to provide the model_name argument to api.predict."""
+    return request.param
+
+
+@pytest.fixture(scope="module", params=[1])
+def version(request):
+    """Fixture to provide the model version argument to api.predict."""
     return request.param
 
 
@@ -36,17 +37,7 @@ def steps(request):
     return request.param
 
 
-@pytest.fixture(scope="module")
-def options(batch_size, steps):
-    """Fixture to return arbitrary keyword options for predictions."""
-    options = {}  # Customize/Complete with predict options
-    options["batch_size"] = batch_size
-    options["steps"] = steps
-    return {k: v for k, v in options.items() if v is not None}
-
-
-@pytest.fixture(scope="module")
-def predictions(model_uri, input_file, accept, options):
-    """Fixture to return predictions to assert properties."""
-    mlflow_uri = api.config.MLFLOW_TRACKING_URI
-    return api.predict(mlflow_uri, model_uri, input_file, accept, **options)
+@pytest.fixture(scope="module", params=["application/json"])
+def accept(request):
+    """Fixture to provide the accept argument to api.predict."""
+    return request.param
